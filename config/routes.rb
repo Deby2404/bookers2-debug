4a2f368f91a0ff73b6882e5db94310c241ca5a7b
Deby2404 <dedede2404@gmail.com>
2022-03-26 14:34:10 +0000

    add following function but there is one error

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,20 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
+
+
   root to: "homes#top"
   get "home/about"=>"homes#about", as: "about"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
   resources :post_comments, only: [:create, :destroy]
   resource :favorites, only: [:create, :destroy]
- end
+  resource :relationships, only: [:create, :destroy]
+  get 'followings' => 'relationships#followings', as: 'followings'
+  get 'followers' => 'relationships#followers', as: 'followers'
+end
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
