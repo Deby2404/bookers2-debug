3bc81fb8cfb1ddfbd4446c7d8168d33233775969
Deby2404 <dedede2404@gmail.com>
2022-04-01 14:38:26 +0000

    missing id in book show

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,12 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get "home/about"=>"homes#about", as: "about"
 
-  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
-  resources :users, only: [:index,:show,:edit,:update]do
+  resources :books, only: [:index,:show,:edit,:create,:destroy,:update]do
+
   resource :favorites, only: [:create, :destroy]
   resources :post_comments, only: [:create, :destroy]
-  get 'followings' => 'relationships#followings', as: 'followings'
-  get 'followers' => 'relationships#followers', as: 'followers'
-end
 
+end
+resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
