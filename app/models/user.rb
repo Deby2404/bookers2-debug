7187ff676d8c79ef6c4a0f50c8d7de17b5addace
Deby2404 <dedede2404@gmail.com>
2022-04-08 12:02:51 +0000

    ゲストユーザーでログインできる機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,6 +43,13 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+  
+  def self.guest
+    find_or_create_by!(name: 'guestuser', email: 'guest@example.com') do |user|
+     user.password = SecureRandom.urlsafe_base64
+     user.name = "guestuser"
+    end  
+  end  
 
 
 end
