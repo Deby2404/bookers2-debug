344423610af9ca9765cd9b77aa428ea28465fb9a
Deby2404 <dedede2404@gmail.com>
2022-04-08 12:33:04 +0000

    ゲストユーザープロフィール編集できないようにする

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:edit]
   before_action :ensure_correct_user, only: [:update, :edit]
 
   def show
@@ -44,5 +45,11 @@ class UsersController < ApplicationController
     unless @user == current_user
       redirect_to user_path(current_user)
     end
+  end
+  
+  def ensure_guest_user
+   if @user = User.find(params[:id])
+    redirect_to user_path(current_user), notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+   end
   end
 end
